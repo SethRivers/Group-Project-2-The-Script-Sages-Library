4c9e44061bb188e8f68b2f29e7c97dc1df520d23
Bodey Lassiter <blassit1@ut.utm.edu>
2024-11-08 18:50:18 -0600

    The library.h file

--- library.h
+++ library.h
@@ -7,14 +7,35 @@
  * 
  */
 
+#ifndef LIBRARY_h
+#DEFINE LIBRARY.H
+
+#include "Book.h"
+#include <list>
+#include <string>
+
 class Library {
- private:
-  struct Book {
-    std::string Title;
-    std::string Author_Name;
-    int Pages;
-    std::string isbn;
-    float Cover_Price;
-    int Year;
-  };
- 
+ public:
+  //loads books from a file
+  void loadFromFile(const std::string& filename);
+
+  //Saves books to a file
+  void saveToFile(const std::string& filename);
+
+  //Adds a book in sorted order by author name
+  void insertSorted(const Book& book);
+
+  //Finds books by author
+  void findAuthor(const std::string& author) const;
+
+  //Finds books by title
+  void findBook(const std::string& author, const std::string& title);
+
+  //Displays all books
+  void displayBooks() const;
+
+private:
+  std::List<Books> books;
+};
+
+#endif
