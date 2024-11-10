7d3d54de2e10ff5780aa51b0a59e2c1c8204840f
Bodey Lassiter <blassit1@ut.utm.edu>
2024-11-09 22:38:01 -0600

    Updated Library.h file

--- library.h
+++ library.h
@@ -1,41 +1,28 @@
-/**
- * @file library.h
- * @author Script Sages
- * @date 2024-10-23
- * @brief This is the .h file for the library. Essentially the library's library that initializes the functions and objects used to run the system. 
- * 
- * 
- */
+#ifndef LIBRARY_H
+#define LIBRARY_H
 
-#ifndef LIBRARY_h
-#DEFINE LIBRARY.H
-
-#include "Book.h"
+#include "book.h"
 #include <list>
 #include <string>
+using namespace std;
 
+// Library class declaration
+// Manages a collection of Book objects stored in a list
 class Library {
- public:
-  //loads books from a file
-  void loadFromFile(const std::string& filename);
-
-  //Saves books to a file
-  void saveToFile(const std::string& filename);
-
-  //Adds a book in sorted order by author name
-  void insertSorted(const Book& book);
-
-  //Finds books by author
-  void findAuthor(const std::string& author) const;
-
-  //Finds books by title
-  void findBook(const std::string& author, const std::string& title);
-
-  //Displays all books
-  void displayBooks() const;
-
 private:
-  std::List<Books> books;
+  list<Book> books; // A list of Book objects to store the library's books
+
+public:
+    // Method declarations
+    void insertSorted(const Book& book);
+    void findAuthor(const string& author) const;
+    void findAlbum(const string& title) const;
+    void deleteBook(const string& author, const string& title);
+    void pushFront(const Book& book);  // Declare pushFront here
+    void pushBack(const Book& book);   // Declare pushBack here
+    void loadFromFile(const string& filename);
+    void saveToFile(const string& filename) const;
+    void displayAllBooks() const;
 };
 
 #endif
